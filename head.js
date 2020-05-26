99d2da59dcdfeb31bf5b2382878e8609816c2a3d
Samantha Gadet <samanthagadet@MacBook-Pro-de-samantha.local>
2020-05-25 20:31:46 -0400

    function head

--- head.js
+++ head.js
@@ -1,15 +1,33 @@
+const pass = String.fromCodePoint(0x2705);
+const fail = String.fromCodePoint(0x274c);
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     if (typeof actual === "string") {
-      console.log(`Assertion Passed: '${actual}' === '${expected}'`);
+      console.log(
+        pass + pass + pass + `Assertion Passed: '${actual}' === '${expected}'`
+      );
     } else {
-      console.log(`Assertion Passed: ${actual} === ${expected}`);
+      console.log(
+        pass + pass + pass + `Assertion Passed: ${actual} === ${expected}`
+      );
     }
   } else if (actual !== expected) {
     if (typeof actual === "string") {
-      console.log(` Assertion Failed: '${actual}' !== '${expected}' `);
+      console.log(
+        fail + fail + fail + `Assertion Failed: '${actual}' !== '${expected}' `
+      );
     } else {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+      console.log(
+        fail + fail + fail + `Assertion Failed: ${actual} !== ${expected}`
+      );
     }
   }
 };
+
+const head = function (array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
