395686426c20711d72dcefaaff47575cdfaaa562
Samy0412 <samantha.gadet@gmail.com>
2020-05-26 23:38:10 +0000

    eqArray function

---
+++ eqArrays.js
@@ -0,0 +1,58 @@
+// FUNCTION IMPLEMENTATION
+const pass = String.fromCodePoint(0x2705);
+const fail = String.fromCodePoint(0x274c);
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    if (typeof actual === "string") {
+      console.log(
+        pass + pass + pass + `Assertion Passed: '${actual}' === '${expected}'`
+      );
+    } else {
+      console.log(
+        pass + pass + pass + `Assertion Passed: ${actual} === ${expected}`
+      );
+    }
+  }
+  if (actual !== expected) {
+    if (typeof actual === "string") {
+      console.log(
+        fail + fail + fail + `Assertion Failed: '${actual}' !== '${expected}' `
+      );
+    } else {
+      console.log(
+        fail + fail + fail + `Assertion Failed: ${actual} !== ${expected}`
+      );
+    }
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  let value = false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      value = true;
+    } else {
+      value = false;
+    }
+  }
+  return value;
+};
+
+// TEST CODE
+//assertEqual();
+//assertEqual();
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
