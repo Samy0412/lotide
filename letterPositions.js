482040c5916b9bcab49d7e9da21690adb01412e5
Samy0412 <samantha.gadet@gmail.com>
2020-05-27 23:15:40 +0000

    letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,66 @@
+const pass = String.fromCodePoint(0x2705);
+const fail = String.fromCodePoint(0x274c);
+
+const assertArraysEqual = function (array1, array2) {
+  let value = false;
+  if (array1.length !== array2.length) {
+    value = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      value = true;
+    } else {
+      value = false;
+    }
+  }
+  if (value === true) {
+    console.log(
+      pass + pass + pass + `Assertion Passed: ${array1} === ${array2}`
+    );
+  } else {
+    console.log(
+      fail + fail + fail + `Assertion Failed:${array1} !== ${array2}`
+    );
+  }
+};
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  let value = false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      value = true;
+    } else {
+      value = false;
+    }
+  }
+  return value;
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  //loop through each letter of the sentence
+  for (let i = 0; i < sentence.length; i++) {
+    //if ' ', ignore it
+    if (sentence[i] !== " ") {
+      //if letter is not in the array, add it as a key and add its index (in a array) as its value
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        //if letter is in the array, push its index in the appropriate key
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+
+//TEST CODE
+result = letterPositions("hello");
+console.log(result);
+assertArraysEqual(result.h, [0]);
+assertArraysEqual(result.e, [1]);
+assertArraysEqual(result.l, [2, 3]);
+assertArraysEqual(result.o, [4]);
