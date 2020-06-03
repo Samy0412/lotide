baf993e6ace0d231f32ff1b23123af4cba9bd3b7
Samy0412 <samantha.gadet@gmail.com>
2020-06-03 01:00:24 +0000

    use of the object index.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,15 @@
 //const assertEqual = require("../assertEqual");
-const tail = require("../tail");
+const _ = require("../index");
 const assert = require("chai").assert;
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const result = _.tail(["Hello", "Lighthouse", "Labs"]);
     assert.deepEqual(result, ["Lighthouse", "Labs"]);
   });
   it("Check the original array", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
-    tail(words);
+    _.tail(words);
     assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"]);
   });
 });
